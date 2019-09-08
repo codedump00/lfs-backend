998e05962164c12d686abe6e9bd75dbf44e4b438
codedump00 <blazinasis@gmail.com>
2019-09-08 17:59:03 +0545

    updated user routes

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 const express = require("express")
 
-// const auth = require("../middlewares/auth")
+const auth = require("../middlewares/auth")
 const user = require("../controllers/users")
 
 const router = express.Router()
@@ -8,5 +8,12 @@ const router = express.Router()
 /* POST methods */
 router.post("/signup/", user.signup)
 router.post("/login/", user.login)
+
+router.get("/", auth, user.findByID)
+router.get("/name/:name", auth, user.findByName)
+
+router.put("/", auth, user.update)
+
+router.delete("/", auth, user.delete)
 
 module.exports = router
