e1d7a8df4b7dc076da606f01a549b31750b56295
codedump00 <blazinasis@gmail.com>
2019-09-25 19:49:26 +0545

    jwt sign secret bug fixed

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -5,7 +5,7 @@ const auth = async (req, res, next) => {
         jwt.verify(
             req.get("x-access-token"),
             // eslint-disable-next-line no-undef
-            process.env.JWT_SECRET,
+            process.env.LFS_SECRET,
             (err, decoded) => {
                 if (err)
                     return res.status(401).json({
