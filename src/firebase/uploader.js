fae4c53817a14158a54e8c239fe06d43cf54b7fb
codedump00 <blazinasis@gmail.com>
2019-11-13 18:24:04 +0545

    changes to update logic

--- src/firebase/uploader.js
+++ src/firebase/uploader.js
@@ -3,42 +3,39 @@ const bucket = require("./firebase");
 const BUCKET_URL = `https://storage.googleapis.com/lfs-cards.appspot.com/`;
 
 const upload = async files => {
-  try {
-    const media = {
-      names: [],
-      src: []
-    };
-    const responses = files.map(async image =>
-      bucket.upload(image.path, { public: true })
-    );
-    await Promise.all(responses).then(response => {
-      response.forEach(res => {
-        console.log(res);
-        media.names.push(res[0].metadata.name);
-        media.src.push(BUCKET_URL + res[0].metadata.name);
-      });
-      files.forEach(image =>
-        // eslint-disable-next-line security/detect-non-literal-fs-filename
-        fs.unlink(image.path, err => {
-          if (err) console.log("Error occured while deleting!");
-        })
-      );
-    });
-    return media;
-  } catch (err) {
-    return { error: err };
-  }
+    try {
+        const media = {
+            names: [],
+            src: []
+        };
+        const responses = files.map(async image =>
+            bucket.upload(image.path, { public: true })
+        );
+        await Promise.all(responses).then(response => {
+            response.forEach(res => {
+                media.names.push(res[0].metadata.name);
+                media.src.push(BUCKET_URL + res[0].metadata.name);
+            });
+            files.forEach(image =>
+                // eslint-disable-next-line security/detect-non-literal-fs-filename
+                fs.unlink(image.path, err => {
+                    if (err) console.log("Error occured while deleting!");
+                })
+            );
+        });
+        return media;
+    } catch (err) {
+        return { error: err };
+    }
 };
 
 const remove = async image => {
-  try {
-    console.log(image);
-    console.log(bucket.file(image));
-    return await bucket.file(image).delete();
-  } catch (e) {
-    console.log(e);
-    return e;
-  }
+    try {
+        console.log(image);
+        return await bucket.file(image).delete();
+    } catch (e) {
+        return e;
+    }
 };
 
 module.exports = { upload, remove };
