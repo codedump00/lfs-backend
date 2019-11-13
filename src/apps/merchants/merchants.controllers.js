793007c7f6832cf408d7636c5868695230b293bf
codedump00 <blazinasis@gmail.com>
2019-11-13 12:31:59 +0545

    debugging image update

--- src/apps/merchants/merchants.controllers.js
+++ src/apps/merchants/merchants.controllers.js
@@ -5,168 +5,167 @@ const Merchant = require("./merchants.models");
 const { upload, remove } = require("../../firebase/uploader");
 
 const create = async (req, res) => {
-    try {
-        const stored = await Merchant.find({ name: req.body.name });
-        if (stored.length > 0)
-            return res.status(409).json({
-                message: "Merchant creation failed. Merchant exists."
-            });
+  try {
+    const stored = await Merchant.find({ name: req.body.name });
+    if (stored.length > 0)
+      return res.status(409).json({
+        message: "Merchant creation failed. Merchant exists."
+      });
 
-        const merchant = Merchant({
-            _id: new mongoose.Types.ObjectId(),
-            name: req.body.name,
-            contact: req.body.contact,
-            address: req.body.address,
-            location: req.body.location,
-            category: req.body.category,
-            timestamp: Date.now()
-        });
-        await merchant.save();
-        return res.status(201).json({
-            message: "Merchant Created",
-            merchant: merchant
-        });
-    } catch (err) {
-        console.log(err);
-        return res.status(400).json({
-            message: "Error creating merchant!",
-            error: err
-        });
-    }
+    const merchant = Merchant({
+      _id: new mongoose.Types.ObjectId(),
+      name: req.body.name,
+      contact: req.body.contact,
+      address: req.body.address,
+      location: req.body.location,
+      category: req.body.category,
+      timestamp: Date.now()
+    });
+    await merchant.save();
+    return res.status(201).json({
+      message: "Merchant Created",
+      merchant: merchant
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(400).json({
+      message: "Error creating merchant!",
+      error: err
+    });
+  }
 };
 
 const get = async (req, res) => {
-    try {
-        const page = req.params.page || 1;
-        const result = 15;
-        const merchants = await Merchant.find(
-            {},
-            "name address location contact category timestamp media"
-        )
-            .skip(result * page - result)
-            .limit(result);
-        return res.status(200).send({
-            result: merchants,
-            count: await Merchant.estimatedDocumentCount()
-        });
-    } catch (err) {
-        return res.status(500).json({
-            error: "Illegal parameters!!"
-        });
-    }
+  try {
+    const page = req.params.page || 1;
+    const result = 15;
+    const merchants = await Merchant.find(
+      {},
+      "name address location contact category timestamp media"
+    )
+      .skip(result * page - result)
+      .limit(result);
+    return res.status(200).send({
+      result: merchants,
+      count: await Merchant.estimatedDocumentCount()
+    });
+  } catch (err) {
+    return res.status(500).json({
+      error: "Illegal parameters!!"
+    });
+  }
 };
 
 const findByID = async (req, res) => {
-    try {
-        const merchant = await Merchant.findById(req.params.id);
-        return res.status(200).send({
-            result: merchant
-        });
-    } catch (err) {
-        return res.status(500).json({
-            error: "Illegal parameters!!"
-        });
-    }
+  try {
+    const merchant = await Merchant.findById(req.params.id);
+    return res.status(200).send({
+      result: merchant
+    });
+  } catch (err) {
+    return res.status(500).json({
+      error: "Illegal parameters!!"
+    });
+  }
 };
 
 const category = async (req, res) => {
-    try {
-        const page = req.params.page || 1;
-        const result = 15;
-        const merchants = await Merchant.find({
-            category: {
-                $regex: RegExp(`${req.params.category}`),
-                $options: "i"
-            }
-        })
-            .skip(result * page - result)
-            .limit(result);
+  try {
+    const page = req.params.page || 1;
+    const result = 15;
+    const merchants = await Merchant.find({
+      category: {
+        $regex: RegExp(`${req.params.category}`),
+        $options: "i"
+      }
+    })
+      .skip(result * page - result)
+      .limit(result);
 
-        return res.status(200).json({
-            result: merchants
-        });
-    } catch (e) {
-        return res.status(400).json({
-            error: "Merchants not found!"
-        });
-    }
+    return res.status(200).json({
+      result: merchants
+    });
+  } catch (e) {
+    return res.status(400).json({
+      error: "Merchants not found!"
+    });
+  }
 };
 
 const update = async (req, res) => {
-    try {
-        await Merchant.findByIdAndUpdate(req.params.id, req.body, {
-            new: true
-        });
-        return res.status(200).json({
-            message: "Merchant updated successfully."
-        });
-    } catch (err) {
-        return res.status(500).json({
-            error: "Error occured!!"
-        });
-    }
+  try {
+    await Merchant.findByIdAndUpdate(req.params.id, req.body, {
+      new: true
+    });
+    return res.status(200).json({
+      message: "Merchant updated successfully."
+    });
+  } catch (err) {
+    return res.status(500).json({
+      error: "Error occured!!"
+    });
+  }
 };
 
 const del = async (req, res) => {
-    try {
-        await Merchant.deleteOne({ _id: req.params.id });
-        return res.status(200).json({
-            message: "Merchant deleted"
-        });
-    } catch (err) {
-        return res.status(400).json({
-            error: "Illegal action!!"
-        });
-    }
+  try {
+    await Merchant.deleteOne({ _id: req.params.id });
+    return res.status(200).json({
+      message: "Merchant deleted"
+    });
+  } catch (err) {
+    return res.status(400).json({
+      error: "Illegal action!!"
+    });
+  }
 };
 
 const imageUpload = async (req, res) => {
-    // req.files is array of `photos` files
-    // req.body will contain the text fields, if there were any
-    try {
-        console.log(req.files, req.file);
-        const media = await upload(req.files);
-        if (media.error)
-            return res.status(400).json({
-                error: "Image uploading failed."
-            });
-        await Merchant.findByIdAndUpdate(req.params.id, {
-            media: media
-        });
-        return res.status(201).json({
-            media: media
-        });
-    } catch (err) {
-        return res.status(400).json({
-            error: "Image uploading failed."
-        });
-    }
+  // req.files is array of `photos` files
+  // req.body will contain the text fields, if there were any
+  try {
+    const media = await upload(req.files);
+    if (media.error)
+      return res.status(400).json({
+        error: "Image uploading failed."
+      });
+    await Merchant.findByIdAndUpdate(req.params.id, {
+      media: media
+    });
+    return res.status(201).json({
+      media: media
+    });
+  } catch (err) {
+    return res.status(400).json({
+      error: "Image uploading failed."
+    });
+  }
 };
 
 const updateImages = async (req, res) => {
-    try {
-        const media = await upload(req.files);
-        if (media.error)
-            return res.status(400).json({
-                error: "Image uploading failed."
-            });
-        const merchant = await Merchant.findById(req.params.id);
-        merchant.media.names.concat(media.names);
-        merchant.media.src.concat(media.src);
-        await merchant.save();
-        return res.status(201).json({
-            media: merchant.media
-        });
-    } catch (err) {}
+  try {
+    const media = await upload(req.files);
+    if (media.error)
+      return res.status(400).json({
+        error: "Image uploading failed."
+      });
+    const merchant = await Merchant.findById(req.params.id);
+    merchant.media.names.push.apply(merchant.media.names, media.names);
+    merchant.media.src.push.apply(merchant.media.src, media.src);
+    await merchant.save();
+    return res.status(201).json({
+      media: merchant.media
+    });
+  } catch (err) {}
 };
 
 module.exports = {
-    create,
-    get,
-    findByID,
-    update,
-    delete: del,
-    upload: imageUpload,
-    category,
-    updateImages
+  create,
+  get,
+  findByID,
+  update,
+  delete: del,
+  upload: imageUpload,
+  category,
+  updateImages
 };
